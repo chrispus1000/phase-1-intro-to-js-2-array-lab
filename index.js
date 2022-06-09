b50c51f639d5ca8b847097c9aa3a0a6db01a4cde
chrispus1000 <minrevac@gmail.com>
2022-06-09 19:11:54 +0300

    commit tree 29b97254f7f268bd3cb8a1ba9ec30080e58df454

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function  destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+
+function prependCat(name){
+    var newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+}
+
+function removeLastCat(){
+    var newArray = cats.slice();
+    newArray.pop()
+    return newArray;
+}
+
+function removeFirstCat(){
+    var newArray = cats.slice();
+    newArray.shift()
+    return newArray;
+}
